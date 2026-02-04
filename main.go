3ed7fb8bbd137cb16664e9ea98b16b9e1e52d1f2
Rin18 <106043571+Rin18@users.noreply.github.com>
2026-02-05 06:40:50 +0800

    Refine Main function

--- main.go
+++ main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"crypto-price-diff-calculator/exchanges"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	ticker, err := exchanges.GetBinanceSymbol("BTCUSDT")
+	//Ask for input
+	var symbol string
+	fmt.Println("Please enter symbol:")
+	fmt.Scan(&symbol)
+	symbol = strings.ToUpper(symbol)
+	//Get Binance Ticker
+	fmt.Println("Binance:")
+	binanceTicker, err := exchanges.GetBinanceSymbol(symbol)
 	if err != nil {
 		fmt.Println("Get symbol failed, error:", err)
 		return
 	}
-	fmt.Println(ticker)
+	fmt.Println(binanceTicker)
+	//Get hyperliquid Ticker
 	fmt.Println("Hyperliquid:")
-	hpreturn, err := exchanges.GetHyperliquidSymbol("BTCUSDT")
+	hypeTicker, err := exchanges.GetHyperliquidSymbol(symbol)
 	if err != nil {
 		fmt.Println("Hyperliquid Error, error:", err)
 		return
 	}
-	fmt.Println(hpreturn)
+	fmt.Println(hypeTicker)
 }
