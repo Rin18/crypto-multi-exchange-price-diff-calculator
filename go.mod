d5cffada3ce10a1f797368f023eab4c29cc6d663
Rin18 <106043571+Rin18@users.noreply.github.com>
2026-02-05 10:10:34 +0800

    Improve overall logic

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module crypto-price-diff-calculator
 
 go 1.25.6
+
+require github.com/shopspring/decimal v1.4.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"crypto-price-diff-calculator/exchanges"
 	"fmt"
 	"strings"
+
+	"github.com/shopspring/decimal"
 )
 
 func main() {
@@ -12,20 +14,53 @@ func main() {
 	fmt.Println("Please enter symbol:")
 	fmt.Scan(&symbol)
 	symbol = strings.ToUpper(symbol)
+
+	// var wg sync.WaitGroup
+	// wg.Add(2)
+
+	binanceResult := make(chan exchanges.Ticker)
+	hypeResult := make(chan exchanges.Ticker)
+
 	//Get Binance Ticker
-	fmt.Println("Binance:")
-	binanceTicker, err := exchanges.GetBinanceSymbol(symbol)
-	if err != nil {
-		fmt.Println("Get symbol failed, error:", err)
-		return
-	}
-	fmt.Println(binanceTicker)
+	go func(symbol string) {
+		// defer wg.Done()
+		binanceTicker, err := exchanges.GetBinanceSymbol(symbol)
+		if err != nil {
+			fmt.Println("Get symbol failed, error:", err)
+			return
+		}
+		binanceResult <- binanceTicker
+	}(symbol)
+
 	//Get hyperliquid Ticker
-	fmt.Println("Hyperliquid:")
-	hypeTicker, err := exchanges.GetHyperliquidSymbol(symbol)
+	go func(symbol string) {
+		// defer wg.Done()
+		hypeTicker, err := exchanges.GetHyperliquidSymbol(symbol)
+		if err != nil {
+			fmt.Println("Hyperliquid Error, error:", err)
+			return
+		}
+		hypeResult <- hypeTicker
+	}(symbol)
+	// wg.Wait()
+	binanceTicker := <-binanceResult
+	hypeTicker := <-hypeResult
+
+	binancePrice, err := decimal.NewFromString(binanceTicker.Price)
 	if err != nil {
-		fmt.Println("Hyperliquid Error, error:", err)
+		fmt.Println("Error decimal convert")
 		return
 	}
-	fmt.Println(hypeTicker)
+	hypePrice, err := decimal.NewFromString(hypeTicker.Price)
+	if err != nil {
+		fmt.Println("Error decimal convert")
+		return
+	}
+	diff := binancePrice.Sub(hypePrice)
+	diff = diff.Abs()
+	percent := diff.DivRound(binancePrice.Add(hypePrice).Div(decimal.NewFromInt(2)), 5)
+	fmt.Printf("Binance %s Price: %s\n", binanceTicker.Symbol, binanceTicker.Price)
+	fmt.Printf("Hype %s Price: %s\n", hypeTicker.Symbol, hypeTicker.Price)
+	fmt.Printf("Absolute diff:%s\n", diff)
+	fmt.Printf("diff percent:%s", percent)
 }
